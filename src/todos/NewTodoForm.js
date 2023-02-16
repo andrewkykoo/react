b25e2c0c709938922549e8283c328e7981b4f88a
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-16 12:14:15 -0500

    add state to keep track of the text input value

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewTodoForm = () => {
+  const [inputValue, setInputValue] = useState('');
   return (
     <div>
-      <input type='text' />
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       <button>create</button>
     </div>
   );
