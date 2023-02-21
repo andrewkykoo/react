59281b9cb3317665b2b9ffe5a741746ce6b2875c
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-20 19:42:35 -0500

    define isLoading reducer and add it to TodoList component

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm';
 import { connect } from 'react-redux';
 import { removeTodo, markCompletedTodo } from './actions';
+import { loadTodos } from './thunks';
 
-const TodoList = ({ todos = [], onRemovePressed, onMarkCompletedPressed }) => {
-  return (
+const TodoList = ({
+  todos = [],
+  onRemovePressed,
+  onMarkCompletedPressed,
+  isLoading,
+  startLoadingTodos,
+}) => {
+  useEffect(() => {
+    startLoadingTodos();
+  }, []);
+
+  const loadingMessage = <div>Loading todos...</div>;
+  const content = (
     <div>
       <NewTodoForm />
       {todos.map((todo, index) => (
@@ -18,15 +30,19 @@ const TodoList = ({ todos = [], onRemovePressed, onMarkCompletedPressed }) => {
       ))}
     </div>
   );
+
+  return isLoading ? loadingMessage : content;
 };
 
 const mapStateToProps = (state) => ({
+  isLoading: state.isLoading,
   todos: state.todos,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeTodo(text)),
   onMarkCompletedPressed: (text) => dispatch(markCompletedTodo(text)),
+  startLoadingTodos: () => dispatch(loadTodos()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
